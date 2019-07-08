c7de1d97ebaf91e243f091f1de8cf89a13ca1395
gontsejnr <gontsemaepa@gmail.com>
2019-07-08 17:33:31 +0200

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,56 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  teamA = 0;
+  teamB = 0;
+  score: number;
+
+  constructor(private router: Router) {}
+
+  //for team A
+  button1_A(){
+    console.log(this.teamA -=3)
+  }
+  button2_A(){
+    console.log(this.teamA +=3)
+  }
+  button3_A(){
+    console.log(this.teamA -=2)
+  }
+  button4_A(){
+    console.log(this.teamA +=2)
+  }
+  button5_A(){
+    console.log(this.teamA -=1)
+  }
+  button6_A(){
+    console.log(this.teamA +=1)
+  }
+
+  //for team B
+  button1_B(){
+    console.log(this.teamB -=3)
+  }
+  button2_B(){
+    console.log(this.teamB +=3)
+  }
+  button3_B(){
+    console.log(this.teamB -=2)
+  }
+  button4_B(){
+    console.log(this.teamB +=2)
+  }
+  button5_B(){
+    console.log(this.teamB -=1)
+  }
+  button6_B(){
+    console.log(this.teamB +=1)
+  }
+
+  reset(){
+    console.log(this.teamA = 0)
+    console.log(this.teamB = 0)
+    this.router.navigateByUrl('/home')
+  }
 
 }
